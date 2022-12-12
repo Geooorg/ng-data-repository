894b1c7447ba6b9a34372e25a5b7490b1b6ad617
Georg Stach <info@GeorgStach.de>
2022-12-12 12:14:42 +0100

    initial commit - blank project from Spring Initializr

---
+++ src/main/kotlin/com/example/datarepository/DataRepositoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.datarepository
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataRepositoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataRepositoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/datarepository/DataRepositoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.datarepository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataRepositoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
